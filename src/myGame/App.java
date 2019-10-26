f1e5a315b4eb0acf7ce864c5d8fbdf9c7d6d177d
ali drake <alidrake01@gmail.com>
2019-10-26 19:01:34 +0100

    added repeating enemies

--- src/myGame/App.java
+++ src/myGame/App.java
@@ -19,13 +19,19 @@ public class App {
             System.out.println("tell us your name");
             String name = sc.nextLine();
             System.out.println("your name is " + name);
-            System.out.println("you have wood: " + items.getWood() + " steel: " + items.getSteel() + " tools: " + items.getTools());
-            while (player.getHP() > 0 && enemy.getHP() > 0) {
+            if (enemy.getHP() < 1) {
+                enemy = new Enemy();
+            }
+
+            while (enemy.getHP() > 0) {
+
+
                 System.out.println("monster has health of " + enemy.getHP());
                 System.out.println("your HP is " + player.getHP());
                 System.out.println("ATTACK");
                 System.out.println("1 for attack");
                 System.out.println("2 for potion heal");
+                System.out.println("3 for item check");
                 int input = sc.nextInt();
                 if (input == 1) {
                     fight(player, enemy);
@@ -35,14 +41,22 @@ public class App {
                         System.out.println("heal for 30HP");
                         player.setHP(player.getHP() + 30);
                         items.setHealthPotion(items.getHealthPotion() - 1);
+                    } else {
+                        System.out.println("No potions left :(");
                     }
 
+                } else if (input == 3) {
+                    itemCheck(items);
                 } else {
                     System.out.println("wrong command");
                 }
+                if (player.getHP() < 1) {
+                    System.out.println(name + " has perished");
+                    System.out.println("***GAME OVER***");
+                    game = false;
+                }
             }
 
-            game = false;
         }
     }
 
@@ -50,8 +64,6 @@ public class App {
     static void fight(Player player, Enemy enemy) {
         enemy.setHP(enemy.getHP() - player.getDMG());
         player.setHP(player.getHP() - enemy.getDMG());
-        System.out.println("monster has hp of " + enemy.getHP());
-        System.out.println("your HP is " + player.getHP());
     }
 
     static void loot(Items items) {
@@ -59,6 +71,14 @@ public class App {
         items.setSteel(items.getSteel() + 5);
         items.setWood(items.getWood() + 10);
         items.setTools(items.getTools() + 1);
+    }
+
+    static void itemCheck(Items items) {
+        System.out.println("you have:"
+                + " \nWood: " + items.getWood()
+                + " \nSteel: " + items.getSteel()
+                + " \nTools: " + items.getTools()
+                + " \nHealthPotions " + items.getHealthPotion());
     }
 
 
