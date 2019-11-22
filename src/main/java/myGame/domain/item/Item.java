5cb471b0314c420ead36fb39d34f2fb397b10bb7
Ali <alidrake01@gmail.com>
2019-11-22 17:31:53 +0000

    started work on using items from inventory, doing this in ItemFactory class...not very clever (#9)

--- src/main/java/myGame/domain/item/Item.java
+++ src/main/java/myGame/domain/item/Item.java
@@ -1,13 +1,18 @@
 package myGame.domain.item;
 
 public interface Item {
+    String name = "";
+    String effect = "";
+    int quantity = 0;
+    int hpBonus = 0;
+    int defBonus = 0;
+    int dmgBonus = 0;
+
     String getName();
-
     int getQuantity();
-
     void setQuantity(int quantity);
-
     String getEffect();
-
-    int getValue();
+    int getHpBonus();
+    int getDefBonus();
+    int getDmgBonus();
 }
